9682d832a02ab2edcdcfe108bef9d5f282310d6e
石井 草太 <ohotori.kuroro1013@gmail.com>
2022-02-23 11:56:20 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "WebController@index");
+    
 
 Route::get("users/carts", "CartController@index")->name("carts.index");
 
