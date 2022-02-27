04e0b0056bbd00ab4a8cdb75b54783354d67e678
石井 草太 <ohotori.kuroro1013@gmail.com>
2022-02-27 01:25:17 +0000

    36 Product Image

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -1,10 +1,11 @@
 <?php
   
   namespace App\Http\Controllers\Dashboard;
-
+  
   use App\Product;
   use App\Category;
   use Illuminate\Http\Request;
+  use Illuminate\Support\Facades\Storage;
   use App\Http\Controllers\Controller;
   
   class ProductController extends Controller
@@ -18,13 +19,13 @@
       {
           $sort_query = [];
           $sorted = "";
- 
+  
           if ($request->sort !== null) {
               $slices = explode(' ', $request->sort);
               $sort_query[$slices[0]] = $slices[1];
               $sorted = $request->sort;
           }
- 
+  
           if ($request->keyword !== null) {
               $keyword = rtrim($request->keyword);
               $total_count = Product::where('name', 'like', "%{$keyword}%")->orwhere('id', "{$keyword}")->count();
@@ -34,14 +35,14 @@
               $total_count = Product::count();
               $products = Product::sortable($sort_query)->paginate(15);
           }
- 
+  
           $sort = [
               '価格の安い順' => 'price asc',
               '価格の高い順' => 'price desc',
               '出品の古い順' => 'updated_at asc',
               '出品の新しい順' => 'updated_at desc'
           ];
- 
+  
           return view('dashboard.products.index', compact('products', 'sort', 'sorted', 'total_count', 'keyword'));
       }
   
@@ -53,7 +54,7 @@
       public function create()
       {
           $categories = Category::all();
- 
+  
           return view('dashboard.products.create', compact('categories'));
       }
   
@@ -75,29 +76,30 @@
               'price.required' => '価格は必須です。',
               'description.required' => '商品説明は必須です。',
           ]);
- 
+
           $product = new Product();
           $product->name = $request->input('name');
           $product->description = $request->input('description');
           $product->price = $request->input('price');
           $product->category_id = $request->input('category_id');
-          if ($request->input("recommend") == "on") {
-            $product->recommend_flag = true;
+          if ($request->input('recommend') == 'on') {
+              $product->recommend_flag = true;
           } else {
-            $product->recommend_flag = false;
+              $product->recommend_flag = false;
           }
-          if ($request->input("carriage") == "on") {
-            $product->carriage_flag = true;
+          if ($request->file('image') !== null) {
+              $image = $request->file('image')->store('public/products');
+              $product->image = basename($image);
           } else {
-            $product->carriage_flag = false;
+              $product->image = '';
           }
           if ($request->input('carriage') == 'on') {
-            $product->carriage_flag = true;
-        } else {
-            $product->carriage_flag = false;
-        }
+              $product->carriage_flag = true;
+          } else {
+              $product->carriage_flag = false;
+          }
           $product->save();
- 
+  
           return redirect()->route('dashboard.products.index');
       }
   
@@ -110,7 +112,7 @@
       public function edit(Product $product)
       {
           $categories = Category::all();
- 
+  
           return view('dashboard.products.edit', compact('product', 'categories'));
       }
   
@@ -133,23 +135,31 @@
               'price.required' => '価格は必須です。',
               'description.required' => '商品説明は必須です。',
           ]);
- 
+
           $product->name = $request->input('name');
           $product->description = $request->input('description');
           $product->price = $request->input('price');
           $product->category_id = $request->input('category_id');
-          if ($request->input("recommend") == "on") {
-            $product->recommend_flag = true;
+          if ($request->input('recommend') == 'on') {
+              $product->recommend_flag = true;
           } else {
-            $product->recommend_flag = false;
+              $product->recommend_flag = false;
           }
-          if ($request->input("carriage") == "on") {
-            $request->carriage_flag = true;
+          if ($request->hasFile('image')) {
+              $image = $request->file('image')->store('public/products');
+              $product->image = basename($image);
+          } else if(isset($product->image)) {
+              // do nothing
           } else {
-            $product->carriage_flag = false;
+              $product->image = '';
+          }
+          if ($request->input('carriage') == 'on') {
+              $product->carriage_flag = true;
+          } else {
+              $product->carriage_flag = false;
           }
           $product->update();
- 
+  
           return redirect()->route('dashboard.products.index');
       }
   
@@ -162,7 +172,7 @@
       public function destroy(Product $product)
       {
           $product->delete();
- 
+  
           return redirect()->route('dashboard.products.index');
       }
   }
